470781538fdcdd90b14230ac07954a973d376924
Emanuel <emvictorel@outlook.com>
2020-06-11 16:54:23 -0300

    Criar a interface básica da aplicação

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/Header'
+import PostList from '../../components/PostList'
+
+function Home(){
+    return (
+        <>
+            <Header/>
+            <PostList/>
+        </>
+    )
+}
+
+export default Home;
